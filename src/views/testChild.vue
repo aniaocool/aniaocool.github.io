b5aacebd604603247d32dc333881855c2f08e552
aniaocool <aniaocool@163.com>
2022-07-20 20:53:05 +0800

    修复格式

--- src/views/testChild.vue
+++ src/views/testChild.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-	<p>修复1.0bug</p>
+    <p>修复1.0bug</p>
     <el-select v-model="inputValue" v-if="isHide">
       <el-option :value="1" :label="1"></el-option>
       <el-option :value="2" :label="2"></el-option>
