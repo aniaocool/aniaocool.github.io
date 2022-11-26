dcbfb63a09cfb22fce334f9f76bc305428c59c6d
aniaocool <aniaocool@163.com>
2022-11-26 17:32:30 +0800

    表单设计器

--- src/views/test.vue
+++ src/views/test.vue
@@ -35,11 +35,15 @@
         <el-button type="primary" @click="onSubmit">提交</el-button>
       </el-form-item>
     </el-form>
+
+    <el-cascader :props="props"></el-cascader>
   </div>
 </template>
 <script>
 import { getInputValue, formatInputValue } from "./amount.js";
 import testChild from "./testChild.vue";
+
+let id = 0;
 
 export default {
   components: { testChild },
@@ -77,6 +81,22 @@ export default {
         ],
       },
       isHide: false,
+      props: {
+        lazy: true,
+        lazyLoad(node, resolve) {
+          console.log(node, resolve);
+          const { level } = node;
+          setTimeout(() => {
+            const nodes = Array.from({ length: level + 1 }).map(() => ({
+              value: ++id,
+              label: `选项${id}`,
+              leaf: level >= 3,
+            }));
+            // 通过调用resolve将子节点数据返回，通知组件数据加载完成
+            resolve(nodes);
+          }, 1000);
+        },
+      },
     };
   },
 
